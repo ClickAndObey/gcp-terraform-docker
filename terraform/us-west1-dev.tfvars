3bef51e9ccd9f2d795eecda0a1a4054fbefe7676
drush211 <thedrush21@gmail.com>
2021-02-10 16:47:59 -0600

    Initialize project with working dockerized terraform image to use for GCP deployments.

---
+++ terraform/us-west1-dev.tfvars
@@ -0,0 +1,2 @@
+region  = "us-west1"
+project = "hello-world-click-and-obey"
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  type        = string
+  description = "The region to deploy to."
+}
+
+variable "project" {
+  type        = string
+  description = "The name of the project."
+}
